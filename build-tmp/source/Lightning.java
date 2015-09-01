1a4b0d7973aa603c8da10eaf923c301c1aba3daa
unknown <rhsharma@697-334-eowyn.sf.k12.internal>
2015-09-01 11:26:20 -0700

    Made lightning vertical.

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,32 +14,32 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-  int startX = 0;
-  int startY = 150;
-  int endX = 0;
-  int endY =150;
+  int startX = 150;
+  int startY = 0;
+  int endX = 150;
+  int endY = 0;
 
 public void setup()
 {
   size(300,300);
   background(0, 0, 0);
-  strokeWeight(5);
+  strokeWeight(2);
 }
 public void draw()
 {
-stroke((int)(Math.random()*255));
-endX = (startX + (int)(Math.random()*9));
-endY = (startY + (int)(Math.random()*18)-9);
+stroke(205, 205, 205);
+endY = (startY + (int)(Math.random()*9));
+endX = (startX + (int)(Math.random()*20)-10);
 line(startX, startY, endX, endY);
 startX = endX;
 startY = endY;
 }
 public void mousePressed()
 {
-startX = 0;
-startY = 150;
-endX = 0;
-endY = 150;
+startX = ((int)(Math.random()*300));
+startY = 0;
+endX = 150;
+endY = 0;
 }
 
   static public void main(String[] passedArgs) {
